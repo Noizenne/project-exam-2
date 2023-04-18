d92c2bccc7b06e75127a62d5bade10051950d92e
Noizenne <karlamae2405@gmail.com>
2023-04-18 23:31:32 +0200

    Working on footer

--- src/Components/Layout/Footer/index.jsx
+++ src/Components/Layout/Footer/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { StyledFooter } from "../../../styles/Footer.styles";
 
-function Footer() {
-  return (
-    <div>Footer</div>
-  )
+export default function Footer() {
+    return (
+      <StyledFooter>
+        <Link to="/"><img src="/logo.jpg" alt="Holidaze logo"/></Link>
+        <h4>About</h4>
+        <h4>Contact us</h4>
+        <p>&copy; Holidaze</p>
+      </StyledFooter>
+    )
 }
-
-export default Footer;
---
+++ src/styles/Footer.styles.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+    
+    img {
+        width: 180px;
+        height: 50px;
+    }
+    h4 {
+        font-size: 16px;
+        font-weight: 300;
+        margin: 5px;
+    }
+
+    p {
+        font-size: 12px;
+    }
+`
